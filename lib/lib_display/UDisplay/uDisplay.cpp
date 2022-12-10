815159c3e5c5409481ef3b851fb915796bcae84e
gemu2015 <gmutz2010@googlemail.com>
2022-12-10 15:56:08 +0100

    prepare RGB displays

--- lib/lib_display/UDisplay/uDisplay.cpp
+++ lib/lib_display/UDisplay/uDisplay.cpp
@@ -24,6 +24,8 @@
 #include "esp8266toEsp32.h"
 #endif
 
+
+
 //#define UDSP_DEBUG
 
 const uint16_t udisp_colors[]={UDISP_BLACK,UDISP_WHITE,UDISP_RED,UDISP_GREEN,UDISP_BLUE,UDISP_CYAN,UDISP_MAGENTA,\
@@ -197,6 +199,36 @@ uDisplay::uDisplay(char *lp) : Renderer(800, 600) {
                 for (uint32_t cnt = 0; cnt < 8; cnt ++) {
                   par_dbh[cnt] = next_val(&lp1);
                 }
+              }
+              spi_speed = next_val(&lp1);
+#endif // USE_ESP32_S3
+              section = 0;
+            }  else if (!strncmp(ibuff, "RGB", 3)) {
+#ifdef USE_ESP32_S3
+              interface = _UDSP_RGB;
+
+              de = next_val(&lp1);
+              vsync = next_val(&lp1);
+              hsync = next_val(&lp1);
+              pclk = next_val(&lp1);
+
+              hsync_polarity = next_val(&lp1);
+              hsync_front_porch = next_val(&lp1);
+              hsync_pulse_width = next_val(&lp1);
+              hsync_back_porch = next_val(&lp1);
+              vsync_polarity = next_val(&lp1);
+              vsync_front_porch = next_val(&lp1);
+              vsync_pulse_width = next_val(&lp1);
+              vsync_back_porch = next_val(&lp1);
+              pclk_active_neg = next_val(&lp1);
+              bpanel = next_val(&lp1);
+
+              for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+                par_dbl[cnt] = next_val(&lp1);
+              }
+
+              for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+                par_dbh[cnt] = next_val(&lp1);
               }
               spi_speed = next_val(&lp1);
 #endif // USE_ESP32_S3
@@ -449,7 +481,28 @@ uDisplay::uDisplay(char *lp) : Renderer(800, 600) {
 #endif // USE_ESP32_S3
 
   }
+  if (interface == _UDSP_RGB) {
+#ifdef USE_ESP32_S3
+    Serial.printf("par  mode: %d\n", hsync_polarity);
+    Serial.printf("par  res: %d\n", hsync_front_porch);
+    Serial.printf("par  cs : %d\n", hsync_pulse_width);
+    Serial.printf("par  rs : %d\n", hsync_back_porch);
+    Serial.printf("par  wr : %d\n", vsync_polarity);
+    Serial.printf("par  rd : %d\n", vsync_front_porch);
+    Serial.printf("par  bp : %d\n", vsync_pulse_width);
+    Serial.printf("par  rd : %d\n", vsync_back_porch);
+    Serial.printf("par  bp : %d\n", pclk_active_neg);
+    Serial.printf("par  bp : %d\n", bpanel);
+    for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+      Serial.printf("par  d%d: %d\n", cnt, par_dbl[cnt]);
+    }
+    for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+      Serial.printf("par  d%d: %d\n", cnt + 8, par_dbh[cnt]);
+    }
+#endif // USE_ESP32_S3
+  }
 #endif
+
 }
 
 
@@ -611,6 +664,67 @@ Renderer *uDisplay::Init(void) {
     SPI_END_TRANSACTION
 
   }
+
+  if (interface == _UDSP_RGB) {
+#ifdef USE_ESP32_S3
+    if (bpanel >= 0) {
+      analogWrite(bpanel, 32);
+    }
+    esp_lcd_rgb_panel_config_t *_panel_config = (esp_lcd_rgb_panel_config_t *)heap_caps_calloc(1, sizeof(esp_lcd_rgb_panel_config_t), MALLOC_CAP_DMA | MALLOC_CAP_INTERNAL);
+
+    _panel_config->clk_src = LCD_CLK_SRC_PLL160M;
+
+    _panel_config->timings.pclk_hz = spi_speed*1000000;
+    _panel_config->timings.h_res = gxs;
+    _panel_config->timings.v_res = gys;
+
+    _panel_config->timings.hsync_pulse_width = hsync_pulse_width;
+    _panel_config->timings.hsync_back_porch = hsync_back_porch;
+    _panel_config->timings.hsync_front_porch = hsync_front_porch;
+    _panel_config->timings.vsync_pulse_width = vsync_pulse_width;
+    _panel_config->timings.vsync_back_porch = vsync_back_porch;
+    _panel_config->timings.vsync_front_porch = vsync_front_porch;
+    _panel_config->timings.flags.hsync_idle_low = (hsync_polarity == 0) ? 1 : 0;
+    _panel_config->timings.flags.vsync_idle_low = (vsync_polarity == 0) ? 1 : 0;
+    _panel_config->timings.flags.de_idle_high = 0;
+    _panel_config->timings.flags.pclk_active_neg = pclk_active_neg;
+    _panel_config->timings.flags.pclk_idle_high = 0;
+
+    _panel_config->data_width = 16; // RGB565 in parallel mode, thus 16bit in width
+    _panel_config->sram_trans_align = 8;
+    _panel_config->psram_trans_align = 64;
+    _panel_config->hsync_gpio_num = hsync;
+    _panel_config->vsync_gpio_num = vsync;
+    _panel_config->de_gpio_num = de;
+    _panel_config->pclk_gpio_num = pclk;
+
+    for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+      _panel_config->data_gpio_nums[cnt] = par_dbl[cnt];
+    }
+    for (uint32_t cnt = 0; cnt < 8; cnt ++) {
+      _panel_config->data_gpio_nums[cnt + 8] = par_dbh[cnt];
+    }
+    _panel_config->disp_gpio_num = GPIO_NUM_NC;
+
+    _panel_config->flags.disp_active_low = 0;
+    _panel_config->flags.relax_on_idle = 0;
+    _panel_config->flags.fb_in_psram = 1;             // allocate frame buffer in PSRAM
+
+    ESP_ERROR_CHECK(esp_lcd_new_rgb_panel(_panel_config, &_panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(_panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(_panel_handle));
+
+    uint16_t color = random(0xffff);
+    ESP_ERROR_CHECK(_panel_handle->draw_bitmap(_panel_handle, 0, 0, 1, 1, &color));
+
+    _rgb_panel = __containerof(_panel_handle, esp_rgb_panel_t, base);
+
+    //return (uint16_t *)_rgb_panel->fb;
+
+#endif // USE_ESP32_S3
+  }
+
+
 
   if (interface == _UDSP_PAR8 || interface == _UDSP_PAR16) {
 
@@ -2789,6 +2903,8 @@ uint32_t uDisplay::get_sr_touch(uint32_t _xp, uint32_t _xm, uint32_t _yp, uint32
 
   return aval;
 }
+
+
 
 
 #if 0
