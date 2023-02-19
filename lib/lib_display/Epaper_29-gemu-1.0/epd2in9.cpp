0558dfa75198b4918ddc8562d04546c73da87d7d
gemu2015 <gmutz2010@googlemail.com>
2023-02-19 14:41:45 +0100

    upd

--- lib/lib_display/Epaper_29-gemu-1.0/epd2in9.cpp
+++ lib/lib_display/Epaper_29-gemu-1.0/epd2in9.cpp
@@ -26,15 +26,6 @@
 
 #include <stdlib.h>
 #include "epd2in9.h"
-#include "tasmota_options.h"
-
-#ifndef EPD_29_V1
-//#define EPD_29_V2
-
-#endif
-
-
-//#define BUSY_PIN 16
 
 
 Epd::Epd(int16_t width, int16_t height) :
@@ -45,35 +36,30 @@ void Epd::DisplayOnff(int8_t on) {
 }
 
 void Epd::Updateframe() {
-#ifdef   EPD_29_V2
-  if (mode == DISPLAY_INIT_PARTIAL) {
-    SetFrameMemory_Partial(framebuffer, 0, 0, EPD_WIDTH,EPD_HEIGHT);
-    DisplayFrame_Partial();
-  } else {
-    SetFrameMemory(framebuffer, 0, 0, EPD_WIDTH,EPD_HEIGHT);
-    DisplayFrame();
-  }
-#else
   SetFrameMemory(framebuffer, 0, 0, EPD_WIDTH,EPD_HEIGHT);
   DisplayFrame();
-#endif
   //Serial.printf("update\n");
 }
 
+#define DISPLAY_INIT_MODE 0
+#define DISPLAY_INIT_PARTIAL 1
+#define DISPLAY_INIT_FULL 2
+
+
 void Epd::DisplayInit(int8_t p,int8_t size,int8_t rot,int8_t font) {
 // ignore update mode
-  if (p == DISPLAY_INIT_PARTIAL) {
+  if (p==DISPLAY_INIT_PARTIAL) {
     Init(lut_partial_update);
     //ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
     DisplayFrame();
-    delay_busy(500);
+    delay(500);
     return;
     //Serial.printf("partial\n");
-  } else if (p == DISPLAY_INIT_FULL) {
+  } else if (p==DISPLAY_INIT_FULL) {
     Init(lut_full_update);
     //ClearFrameMemory(0xFF);   // bit set = white, bit reset = black
     DisplayFrame();
-    delay_busy(3500);
+    delay(3500);
     //Serial.printf("full\n");
     return;
   } else {
@@ -92,31 +78,26 @@ void Epd::DisplayInit(int8_t p,int8_t size,int8_t rot,int8_t font) {
   disp_bpp = 1;
 }
 
-void Epd::Begin(int16_t cs,int16_t mosi,int16_t sclk, int16_t rst, int16_t busy) {
-  cs_pin = cs;
-  mosi_pin = mosi;
-  sclk_pin = sclk;
-  rst_pin = rst;
-  busy_pin = busy;
-#ifdef BUSY_PIN
-  busy_pin = BUSY_PIN;
-#endif
+void Epd::Begin(int16_t cs,int16_t mosi,int16_t sclk,int16_t reset,int16_t busy) {
+  cs_pin=cs;
+  mosi_pin=mosi;
+  sclk_pin=sclk;
+  reset_pin=reset;
+  busy_pin=busy;
 }
 
-
 void Epd::Init(int8_t p) {
-  if (p == DISPLAY_INIT_PARTIAL) {
+  if (p==DISPLAY_INIT_PARTIAL) {
     Init(lut_partial_update);
   } else {
     Init(lut_full_update);
   }
-  mode = p;
   ClearFrameMemory(0xFF);
   DisplayFrame();
-  if (p == DISPLAY_INIT_PARTIAL) {
-    delay_busy(350);
+  if (p==DISPLAY_INIT_PARTIAL) {
+    delay(350);
   } else {
-    delay_busy(3500);
+    delay(3500);
   }
 }
 
@@ -132,9 +113,6 @@ int Epd::Init(const unsigned char* lut) {
     sclk_pin=pin[GPIO_SSPI_SCLK];
 */
 
-    if (framebuffer) {
-    //  free(framebuffer);
-    }
     framebuffer = (uint8_t*)malloc(EPD_WIDTH * EPD_HEIGHT / 8);
     if (!framebuffer) return -1;
 
@@ -146,46 +124,9 @@ int Epd::Init(const unsigned char* lut) {
     digitalWrite(mosi_pin,LOW);
     digitalWrite(sclk_pin,LOW);
 
-    if (rst_pin >= 0) {
-      pinMode(rst_pin, OUTPUT);
-      digitalWrite(rst_pin, HIGH);
-    }
-
-    if (busy_pin >= 0) {
-      pinMode(busy_pin, INPUT_PULLUP);
-    }
-
     width = EPD_WIDTH;
     height = EPD_HEIGHT;
 
-#ifdef EPD_29_V2
-    /* EPD hardware init start */
-    Reset();
-
-    SendCommand(0x12);  //SWRESET
-    delay_busy(100);
-
-    SendCommand(0x01); //Driver output control
-    SendData(0x27);
-    SendData(0x01);
-    SendData(0x00);
-
-    SendCommand(0x11); //data entry mode
-    SendData(0x03);
-
-    SetMemoryArea(0, 0, width-1, height-1);
-
-    SendCommand(0x21); //  Display update control
-    SendData(0x00);
-    SendData(0x80);
-
-    SetMemoryPointer(0, 0);
-    delay_busy(10);
-
-    SetLut_by_host(lut_full_update);
-    mode = DISPLAY_INIT_FULL;
-
-#else
     /* EPD hardware init start */
     this->lut = lut;
     Reset();
@@ -206,7 +147,6 @@ int Epd::Init(const unsigned char* lut) {
     SendCommand(DATA_ENTRY_MODE_SETTING);
     SendData(0x03);                     // X increment; Y increment
     SetLut(this->lut);
-#endif
     /* EPD hardware init end */
     return 0;
 }
@@ -229,18 +169,15 @@ void Epd::SendData(unsigned char data) {
   //  SpiTransfer(data);
 }
 
-
-
-void Epd::delay_busy(uint32_t wait) {
-  if (busy_pin >= 0) {
-    while (digitalRead(busy_pin) == HIGH) {      //LOW: idle, HIGH: busy
-      delay(10);
-    }
-  } else {
-    delay(wait);
-  }
+/**
+ *  @brief: Wait until the busy_pin goes LOW
+ */
+void Epd::WaitUntilIdle(void) {
+  return;
+    //while(DigitalRead(busy_pin) == HIGH) {      //LOW: idle, HIGH: busy
+    //    DelayMs(100);
+    //}
 }
-
 
 /**
  *  @brief: module reset.
@@ -248,44 +185,12 @@ void Epd::delay_busy(uint32_t wait) {
  *          see Epd::Sleep();
  */
 void Epd::Reset(void) {
-    if (rst_pin >= 0) {
-      digitalWrite(rst_pin, LOW);                //module reset
-      delay(200);
-      digitalWrite(rst_pin, HIGH);
-      delay(200);
-    } else {
-      SendCommand(0x12);
-    }
+    //DigitalWrite(reset_pin, LOW);                //module reset
+    //delay(200);
+    //DigitalWrite(reset_pin, HIGH);
+    //delay(200);
 }
 
-#ifdef EPD_29_V2
-void Epd::SetLut(const unsigned char *lut) {
-	unsigned char count;
-	SendCommand(0x32);
-	for(count=0; count<153; count++)
-		SendData(lut[count]);
-	delay_busy(50);
-}
-
-
-void Epd::SetLut_by_host(const unsigned char *lut) {
-  SetLut((unsigned char *)lut);
-	SendCommand(0x3f);
-	SendData(*(lut+153));
-	SendCommand(0x03);	// gate voltage
-	SendData(*(lut+154));
-	SendCommand(0x04);	// source voltage
-	SendData(*(lut+155));	// VSH
-	SendData(*(lut+156));	// VSH2
-	SendData(*(lut+157));	// VSL
-	SendCommand(0x2c);		// VCOM
-	SendData(*(lut+158));
-}
-#else
-
-void Epd::SetLut_by_host(const unsigned char *lut)  {
-
-}
 /**
  *  @brief: set the look-up table register
  */
@@ -297,7 +202,6 @@ void Epd::SetLut(const unsigned char* lut) {
         SendData(this->lut[i]);
     }
 }
-#endif
 
 /**
  *  @brief: put an image buffer to the frame memory.
@@ -310,7 +214,6 @@ void Epd::SetFrameMemory(
     uint16_t image_width,
     uint16_t image_height
 ) {
-
     uint16_t x_end;
     uint16_t y_end;
 
@@ -401,118 +304,13 @@ void Epd::ClearFrameMemory(unsigned char color) {
  *          set the other memory area.
  */
 void Epd::DisplayFrame(void) {
-    SendCommand(DISPLAY_UPDATE_CONTROL_2); // 0x22
-#ifdef EPD_29_V2
-    SendData(0xC7);
-#else
+    SendCommand(DISPLAY_UPDATE_CONTROL_2);
     SendData(0xC4);
-#endif
-    SendCommand(MASTER_ACTIVATION); // 0x20
-#ifndef EPD_29_V2
+    SendCommand(MASTER_ACTIVATION);
     SendCommand(TERMINATE_FRAME_READ_WRITE);
-#endif
-    delay_busy(10);
+    WaitUntilIdle();
 }
 
-void Epd::DisplayFrame_Partial(void) {
-    SendCommand(0x22);
-    SendData(0x0F);
-    SendCommand(0x20);
-    delay_busy(10);
-}
-
-#ifdef EPD_29_V2
-
-void Epd::SetFrameMemory_Partial(const unsigned char* image_buffer, int x, int y, int image_width, int image_height) {
-    int x_end;
-    int y_end;
-
-    if (
-        image_buffer == NULL ||
-        x < 0 || image_width < 0 ||
-        y < 0 || image_height < 0
-    ) {
-        return;
-    }
-    /* x point must be the multiple of 8 or the last 3 bits will be ignored */
-    x &= 0xF8;
-    image_width &= 0xF8;
-    if (x + image_width >= this->width) {
-        x_end = this->width - 1;
-    } else {
-        x_end = x + image_width - 1;
-    }
-    if (y + image_height >= this->height) {
-        y_end = this->height - 1;
-    } else {
-        y_end = y + image_height - 1;
-    }
-
-    if (rst_pin >= 0) {
-      digitalWrite(rst_pin, LOW);
-      delay(2);
-      digitalWrite(rst_pin, HIGH);
-      delay(2);
-    } else {
-      SendCommand(0x12);
-    }
-	SetLut(lut_partial_update);
-	SendCommand(0x37);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x40);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x00);
-	SendData(0x00);
-
-	SendCommand(0x3C); //BorderWavefrom
-	SendData(0x80);
-
-	SendCommand(0x22);
-	SendData(0xC0);
-	SendCommand(0x20);
-	delay_busy(100);
-
-  SetMemoryArea(x, y, x_end, y_end);
-  SetMemoryPointer(x, y);
-  SendCommand(0x24);
-  /* send the image data */
-  for (int j = 0; j < y_end - y + 1; j++) {
-    for (int i = 0; i < (x_end - x + 1) / 8; i++) {
-        SendData(image_buffer[i + j * (image_width / 8)]^0xff);
-    }
-  }
-}
-
-
-/**
- *  @brief: private function to specify the memory area for data R/W
- */
-void Epd::SetMemoryArea(int x_start, int y_start, int x_end, int y_end) {
-    SendCommand(0x44);
-    /* x point must be the multiple of 8 or the last 3 bits will be ignored */
-    SendData((x_start >> 3) & 0xFF);
-    SendData((x_end >> 3) & 0xFF);
-    SendCommand(0x45);
-    SendData(y_start & 0xFF);
-    SendData((y_start >> 8) & 0xFF);
-    SendData(y_end & 0xFF);
-    SendData((y_end >> 8) & 0xFF);
-}
-#else
-
-void Epd::SetFrameMemory_Partial(
-    const unsigned char* image_buffer,
-    int x,
-    int y,
-    int image_width,
-    int image_height
-) {
-}
 /**
  *  @brief: private function to specify the memory area for data R/W
  */
@@ -527,24 +325,7 @@ void Epd::SetMemoryArea(int x_start, int y_start, int x_end, int y_end) {
     SendData(y_end & 0xFF);
     SendData((y_end >> 8) & 0xFF);
 }
-#endif
 
-
-#ifdef EPD_29_V2
-
-/**
- *  @brief: private function to specify the start point for data R/W
- */
-void Epd::SetMemoryPointer(int x, int y) {
-    SendCommand(0x4E);
-    /* x point must be the multiple of 8 or the last 3 bits will be ignored */
-    SendData((x >> 3) & 0xFF);
-    SendCommand(0x4F);
-    SendData(y & 0xFF);
-    SendData((y >> 8) & 0xFF);
-    delay_busy(10);
-}
-#else
 /**
  *  @brief: private function to specify the start point for data R/W
  */
@@ -555,9 +336,8 @@ void Epd::SetMemoryPointer(int x, int y) {
     SendCommand(SET_RAM_Y_ADDRESS_COUNTER);
     SendData(y & 0xFF);
     SendData((y >> 8) & 0xFF);
-    delay_busy(10);
+    WaitUntilIdle();
 }
-#endif
 
 /**
  *  @brief: After this command is transmitted, the chip would enter the
@@ -567,58 +347,8 @@ void Epd::SetMemoryPointer(int x, int y) {
  */
 void Epd::Sleep() {
     SendCommand(DEEP_SLEEP_MODE);
-    delay_busy(10);
+    WaitUntilIdle();
 }
-
-#ifdef EPD_29_V2
-
-const unsigned char lut_partial_update[159] =
-{
-0x0,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x80,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x40,0x40,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x80,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0A,0x0,0x0,0x0,0x0,0x0,0x2,
-0x1,0x0,0x0,0x0,0x0,0x0,0x0,
-0x1,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x0,0x0,0x0,0x0,0x0,0x0,0x0,
-0x22,0x22,0x22,0x22,0x22,0x22,0x0,0x0,0x0,
-0x22,0x17,0x41,0xB0,0x32,0x36,
-};
-
-const unsigned char lut_full_update[159] =
-{
-0x80,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x40,	0x0,	0x0,	0x0,
-0x10,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x20,	0x0,	0x0,	0x0,
-0x80,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x40,	0x0,	0x0,	0x0,
-0x10,	0x66,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x20,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x14,	0x8,	0x0,	0x0,	0x0,	0x0,	0x1,
-0xA,	0xA,	0x0,	0xA,	0xA,	0x0,	0x1,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x14,	0x8,	0x0,	0x1,	0x0,	0x0,	0x1,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x1,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x0,	0x0,	0x0,	0x0,	0x0,	0x0,	0x0,
-0x44,	0x44,	0x44,	0x44,	0x44,	0x44,	0x0,	0x0,	0x0,
-0x22,	0x17,	0x41,	0x0,	0x32,	0x36
-};
-
-#else
 
 const unsigned char lut_full_update[] =
 {
@@ -635,7 +365,7 @@ const unsigned char lut_partial_update[] =
     0x00, 0x00, 0x00, 0x00, 0x13, 0x14, 0x44, 0x12,
     0x00, 0x00, 0x00, 0x00, 0x00, 0x00
 };
-#endif // EPD_29_V2
+
 
 #define PIN_OUT_SET 0x60000304
 #define PIN_OUT_CLEAR 0x60000308
