1e9d96ee676fd21754e0b90b1591dbb8325d7ec4
gemu2015 <gmutz2010@googlemail.com>
2023-03-17 09:05:35 +0100

    remove wolf

--- lib/libesp32_div/wolfssl/src/wolfssl.h
+++
@@ -1,9 +0,0 @@
-/***************************************************************************//**
- * @file    wolfssl.h
- * @brief   Arduino wolfSSL library header
- * @author  onelife <onelife.real[at]gmail.com>
- ******************************************************************************/
-#ifndef __WOLFSSL_H__
-#define __WOLFSSL_H__
-
-#endif /* __WOLFSSL_H__ */
