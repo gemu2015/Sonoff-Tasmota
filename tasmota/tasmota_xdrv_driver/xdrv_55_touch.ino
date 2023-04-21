7206355699c25e54b8bbb7fe07969acb3632ae42
gemu2015 <gmutz2010@googlemail.com>
2023-04-21 09:08:58 +0200

    Update xdrv_55_touch.ino

--- tasmota/tasmota_xdrv_driver/xdrv_55_touch.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_55_touch.ino
@@ -256,16 +256,7 @@ bool XPT2046_Touch_Init(uint16_t CS, int8_t irqpin, uint8_t bus) {
   uint8_t xbus = bus;
   bus &= 1;
  #ifdef ESP32
-
-// strange compiler bug !!!
-// this works
-  bool p1 = PinUsed(GPIO_SPI_CLK, bus);
-  bool p2 = PinUsed(GPIO_SPI_MISO, bus);
-  bool p3 = PinUsed(GPIO_SPI_MOSI, bus);
-  if (p1 && p2 && p3) {
-
-// this does not work 
- //if (PinUsed(GPIO_SPI_CLK, bus) && PinUsed(GPIO_SPI_MISO, bus) && PinUsed(GPIO_SPI_MOSI), bus) {
+ if (PinUsed(GPIO_SPI_CLK, bus) && PinUsed(GPIO_SPI_MISO, bus) && PinUsed(GPIO_SPI_MOSI, bus)) {
     // must init SPI with pins
     sclk = Pin(GPIO_SPI_CLK, bus);
     miso = Pin(GPIO_SPI_MISO, bus);
