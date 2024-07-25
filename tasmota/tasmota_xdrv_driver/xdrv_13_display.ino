8f1e71fd3f9601e2498eb95ae68b784089ceedca
gemu2015 <gmutz2010@googlemail.com>
2024-07-25 09:51:40 +0200

    Update xdrv_13_display.ino

--- tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
@@ -2455,6 +2455,9 @@ void Draw_RGB_Bitmap(char *file, uint16_t xp, uint16_t yp, uint8_t scale, bool i
 #ifdef ESP32
 #ifdef JPEG_PICTS
 #define JPG_DEFSIZE 150000
+
+void Draw_jpeg(uint8_t *mem, uint16_t jpgsize, uint16_t xp, uint16_t yp, uint8_t scale);
+
 void Draw_JPG_from_URL(char *url, uint16_t xp, uint16_t yp, uint8_t scale) {
   uint8_t *mem = 0;
   WiFiClient http_client;
