9e8dbbf1b814429e50a0e7fad7d620ae7c955410
gemu2015 <gmutz2010@googlemail.com>
2024-08-27 16:44:03 +0200

    upd

--- tasmota/tasmota_xdrv_driver/xdrv_123_plugins.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_123_plugins.ino
@@ -2517,6 +2517,18 @@ void Check_partition(void) {
   }
 
   // partition talble is aways at 0x8000
+/*  typedef struct {
+    esp_flash_t* flash_chip;            //!< SPI flash chip on which the partition resides
+    esp_partition_type_t type;          //!< partition type (app/data)
+    esp_partition_subtype_t subtype;    //!< partition subtype
+    uint32_t address;                   //!< starting address of the partition in flash
+    uint32_t size;                      //!< size of the partition, in bytes
+    uint32_t erase_size;                //!< size the erase operation should be aligned to
+    char label[17];                     //!< partition label, zero-terminated ASCII string
+    bool encrypted;                     //!< flag is set to true if partition is encrypted
+    bool readonly;                      //!< flag is set to true if partition is read-only
+} esp_partition_t;
+*/
   esp_partition_t spiffs;
 
   esp_partition_iterator_t iterator = NULL;
