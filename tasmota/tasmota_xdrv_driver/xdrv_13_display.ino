e39b6360b34d49ccf07d4e1b2ff51e2630488106
gemu2015 <gmutz2010@googlemail.com>
2025-09-23 13:13:25 +0200

    upd

--- tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_13_display.ino
@@ -2404,7 +2404,7 @@ char ppath[16];
 #ifdef JPEG_PICTS
 #include "img_converters.h"
 #include "jpeg_decoder.h"
-#include "soft_esp_jpg_decode.h"
+#include "esp_jpg_decode.h"
 bool jpg2rgb888(const uint8_t *src, size_t src_len, uint8_t * out, jpg_scale_t scale);
 bool jpg2rgb565(const uint8_t *src, size_t src_len, uint8_t * out, jpg_scale_t scale);
 char get_jpeg_size(unsigned char* data, unsigned int data_size, unsigned short *width, unsigned short *height);
