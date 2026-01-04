df149c4395342d065240d0a4464323d7a3fc55d0
Gerhard Mutz <gerhardmutz1@imac.local>
2026-01-04 11:24:08 +0100

    upd

--- tasmota/tasmota_xdrv_driver/xdrv_97_esp32_m5epd47.ino
+++ tasmota/tasmota_xdrv_driver/xdrv_97_esp32_m5epd47.ino
@@ -87,14 +87,62 @@ void M5EPDRtcInit(void) {
 void M5EPDModuleInit(void) {
   if (!TasmotaGlobal.i2c_enabled[1]) {
     //I2c2Begin(21, 22, 400000);
-    I2cBegin(21, 22, 1);
+    //I2cBegin(21, 22, 1);
   }
-  M5EPD_globs.m5epd.begin();
+  
+  //M5EPD_globs.m5epd.begin();
+
+/*
+#define M5EPD_MAIN_PWR_PIN 2
+#define M5EPD_CS_PIN 15
+#define M5EPD_SCK_PIN 14
+#define M5EPD_MOSI_PIN 12
+#define M5EPD_BUSY_PIN 27
+#define M5EPD_MISO_PIN 13
+#define M5EPD_EXT_PWR_EN_PIN 5
+#define M5EPD_EPD_PWR_EN_PIN 23
+#define M5EPD_KEY_RIGHT_PIN 39
+#define M5EPD_KEY_PUSH_PIN 38
+#define M5EPD_KEY_LEFT_PIN 37
+#define M5EPD_BAT_VOL_PIN 35
+#define M5EPD_PORTC_W_PIN 19
+#define M5EPD_PORTC_Y_PIN 18
+#define M5EPD_PORTB_W_PIN 33
+#define M5EPD_PORTB_Y_PIN 26
+#define M5EPD_PORTA_W_PIN 32
+#define M5EPD_PORTA_Y_PIN 25
+
+#define M5EPD_CS_SD_PIN 4
+*/
+
+#define enableEXTPower() digitalWrite(M5EPD_EXT_PWR_EN_PIN, 1)
+#define enableEPDPower() digitalWrite(M5EPD_EPD_PWR_EN_PIN, 1)
+#define enableMainPower() digitalWrite(M5EPD_MAIN_PWR_PIN, 1)
+
+  pinMode(M5EPD_MAIN_PWR_PIN, OUTPUT);
+  enableMainPower();
+
+  pinMode(M5EPD_CS_PIN, OUTPUT);
+  digitalWrite(M5EPD_CS_PIN, 1);
+
+  //pinMode(M5EPD_CS_SD_PIN, OUTPUT);
+  //digitalWrite(M5EPD_CS_SD_PIN, 1);
+
+  pinMode(M5EPD_EXT_PWR_EN_PIN, OUTPUT);
+  pinMode(M5EPD_EPD_PWR_EN_PIN, OUTPUT);
+
+  pinMode(M5EPD_KEY_RIGHT_PIN, INPUT);
+  pinMode(M5EPD_KEY_PUSH_PIN, INPUT);
+  pinMode(M5EPD_KEY_LEFT_PIN, INPUT);
   delay(100);
+
+  enableEPDPower();
+ // enableEXTPower();
   AddLog(LOG_LEVEL_INFO, PSTR("DRV: M5 E-Paper 4.7"));
 
   // must reinitialyze SD card
-  UfsCheckSDCardInit();
+  //SPI.end();
+  //UfsCheckSDCardInit();
 
   M5EPD_globs.ready = true;
 }
@@ -112,9 +160,13 @@ void M5EPDEverySecond(void) {
 
 #define BATTERY "Battery"
 
+#define BAT_ADC_CHANNEL 35
+
 void M5EPDShow(uint32_t json) {
   if (!M5EPD_globs.ready) return;
-  float bvolt = (float)M5EPD_globs.m5epd.getBatteryVoltage()/1000.0;
+  //float bvolt = (float)M5EPD_globs.m5epd.getBatteryVoltage()/1000.0;
+  float bvolt = (float)analogReadMilliVolts(BAT_ADC_CHANNEL)/500.0;
+
   if (json) {
     ResponseAppend_P(PSTR(",\"M5EPD\":{\"BV\":%3_f}"), &bvolt);
   } else {
@@ -127,10 +179,15 @@ void M5EPDShow(uint32_t json) {
 \*********************************************************************************************/
 
 const char kM5EPDCommands[] PROGMEM = "M5EPD|"
-  "Shutdown";
+  "Shutdown|Init";
 
 void (* const M5EPDCommand[])(void) PROGMEM = {
-  &CmndM5EPDShutdown};
+  &CmndM5EPDShutdown,&M5EPDMInit};
+
+void M5EPDMInit(void) {
+  enableEXTPower();
+  ResponseCmndDone();
+}
 
 void CmndM5EPDShutdown(void) {
   char *mp = strchr(XdrvMailbox.data, ':');
@@ -160,7 +217,7 @@ bool Xdrv97(uint32_t function) {
 
   switch (function) {
     case FUNC_EVERY_SECOND:
-      M5EPDEverySecond();
+      //M5EPDEverySecond();
       break;
     case FUNC_JSON_APPEND:
       M5EPDShow(1);
@@ -174,8 +231,8 @@ bool Xdrv97(uint32_t function) {
       result = DecodeCommand(kM5EPDCommands, M5EPDCommand);
       break;
     case FUNC_INIT:
-      M5EPDRtcInit();
-      break;
+      //M5EPDRtcInit();
+      //break;
     case FUNC_MODULE_INIT:
       M5EPDModuleInit();
       break;
